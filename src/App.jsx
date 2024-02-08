6ae2ad57e640acef934b41143a1d843e1b1cc7b0
user.email <jobayer.ahmed109@gmail.com>
2024-02-08 23:19:16 +0600

    Initial jsx created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <h1>JSON to JSX to JSON</h1>;
+  return (
+    <div>
+      <h2>JSON to JSX to JSON</h2>
+      <div>
+        <h3 style={{ margin: "1rem" }}>Result: 0</h3>
+        <div className="input__div">
+          <input style={{ marginBottom: "5px" }} type="number" /> <br />
+          <input type="number" />
+        </div>
+      </div>
+      <div className="btn-group">
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button> <br />
+        <button className="reset">Reset</button>
+      </div>
+      <div className="hr__line"></div>
+      <div style={{ marginTop: "1rem" }}>
+        <h3>History</h3>
+        <p>
+          <small>No history here</small>
+        </p>
+      </div>
+    </div>
+  );
 }
 export default App;
