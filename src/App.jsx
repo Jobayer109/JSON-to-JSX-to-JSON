ee8a6b90dcf3d190cbc0f3d9b4e12f9280370dbd
user.email <jobayer.ahmed109@gmail.com>
2024-02-09 00:56:19 +0600

    Handle operation using Function constructor and with eval()

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-
+/*
+TODO: Handle user input fields    -----> Done
+TODO: Handle operations           -----> Done
+TODO: Handle a list of histories  ----->
+TODO: Render history list         ----->
+TODO: Restore the history         ----->
+*/
 const inputObj = {
   a: 0,
   b: 0,
@@ -8,19 +14,39 @@ const inputObj = {
 
 function App() {
   const [inputState, setInputState] = useState(inputObj);
+  const [result, setResult] = useState(0);
 
   const handleChange = (e) => {
     setInputState({
       ...inputState,
-      [e.target.name]: e.target.value,
+      [e.target.name]: parseInt(e.target.value),
     });
+  };
+
+  const handleArithmeticOps = (operator) => {
+    // handle by creating function on the fly
+    const opsFunc = new Function(
+      "operator",
+      `return ${inputState.a} ${operator} ${inputState.b}`
+    );
+    setResult(opsFunc(operator));
+
+    // handle using eval()
+    // const res = eval(`${inputState.a} ${operator} ${inputState.b}`);
+    // setResult(res);
+    // console.log(res);
+  };
+
+  const handleReset = () => {
+    setInputState(inputObj);
+    setResult(0);
   };
 
   return (
     <div>
       <h2>JSON to JSX to JSON</h2>
       <div>
-        <h3 style={{ margin: "1rem" }}>Result: 0</h3>
+        <h3 style={{ margin: "1rem" }}>Result: {result}</h3>
         <div className="input__div">
           <input
             type="number"
@@ -38,11 +64,13 @@ function App() {
         </div>
       </div>
       <div className="btn-group">
-        <button>+</button>
-        <button>-</button>
-        <button>*</button>
-        <button>/</button> <br />
-        <button className="reset">Reset</button>
+        <button onClick={() => handleArithmeticOps("+")}>+</button>
+        <button onClick={() => handleArithmeticOps("-")}>-</button>
+        <button onClick={() => handleArithmeticOps("*")}>*</button>
+        <button onClick={() => handleArithmeticOps("/")}>/</button> <br />
+        <button onClick={handleReset} className="reset">
+          Reset
+        </button>
       </div>
       <div className="hr__line"></div>
       <div style={{ marginTop: "1rem" }}>
